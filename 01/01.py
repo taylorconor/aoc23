eef8ac074f566f716cec716f41c966ea2840e6ed
Conor Taylor <taylorconor@gmail.com>
2023-12-01 00:46:25 -0500

    Day 01

--- 01/01.py
+++ 01/01.py
@@ -3,4 +3,9 @@ import os
 with open(os.path.join(os.path.dirname(__file__), 'input.txt')) as f:
     lines = [x.strip() for x in f.read().splitlines()]
 
-print("01")
+s = 0
+for line in lines:
+    f = ''.join(x for x in line if x.isdigit())
+    s += int(f[0] + f[-1])
+
+print("Sum = " + str(s))
--- 01/02.py
+++ 01/02.py
@@ -3,4 +3,27 @@ import os
 with open(os.path.join(os.path.dirname(__file__), 'input.txt')) as f:
     lines = [x.strip() for x in f.read().splitlines()]
 
-print("02")
+values = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+}
+
+ss = 0
+for line in lines:
+    s = ""
+    for i, c in enumerate(line):
+        if c.isdigit():
+            s += c
+        for n in values:
+            if line[i:].startswith(n):
+                s += values[n]
+    ss += int(s[0] + s[-1])
+
+print("Sum = " + str(ss))
